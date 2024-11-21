b91359d03cec59de288e21d9ad7ddd5741c215ed
Олег Мальцев <oyumaltsev@sberbank.ru>
2024-11-21 16:51:34 +0300

    FIX comment erasure

--- crypto/keypair_test.go
+++ crypto/keypair_test.go
@@ -9,7 +9,6 @@ import (
 func TestGeneratePrivateKey(t *testing.T){
 	privKey := GeneratePrivateKey()
 	pubKey := privKey.PublicKey()
-	// address := pubKey.Address()
 
 	msg := []byte("hello world")
 	sig, err := privKey.Sign(msg)
